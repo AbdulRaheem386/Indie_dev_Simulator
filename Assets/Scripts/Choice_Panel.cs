f86b587f1547186041a94df76d4b03a3f151d9f0
Hafiz Abdul Raheem <chhafizidrees123@gmail.com>
2026-09-03 11:11:41 +0500

    Prototype Complete

--- Assets/Scripts/Choice_Panel.cs
+++ Assets/Scripts/Choice_Panel.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Choice_Panel : MonoBehaviour
 {
     public GameObject Choseoption;
-
+    public Scenes_Manager scenes_manager;
     public Bank_Manager bank_manager;
+    public GameObject RestratButton;
 
     public void choice()
     {
         Choseoption.SetActive(true);
         bank_manager.Submoney(2000);
+    }
+
+    public void Restart()
+    {
+        scenes_manager.Scene_Change();
     }
 }
